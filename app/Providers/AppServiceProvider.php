99afcd6c573c6430dd5c03ea49a4000d395c5c9b
PGerg03 <rpgergo@gmail.com>
2025-04-09 08:06:27 +0200

    F9 teljesen

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,30 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\User;
+use App\Models\Device;
+use App\Models\Document;
+use App\Models\DeviceType;
+use App\Policies\RolePolicy;
+use App\Policies\UserPolicy;
+use App\Policies\DevicePolicy;
+use App\Policies\DocumentPolicy;
+use App\Policies\DeviceTypePolicy;
+use App\Policies\PermissionPolicy;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class AppServiceProvider extends ServiceProvider
 {
+    protected $policies = [
+        Permission::class => PermissionPolicy::class,
+        Role::class => RolePolicy::class,
+        User::class => UserPolicy::class,
+        DeviceType::class => DeviceTypePolicy::class,
+        Device::class => DevicePolicy::class,
+        Document::class => DocumentPolicy::class,
+    ];
+
     /**
      * Register any application services.
      */
